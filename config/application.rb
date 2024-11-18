58f069febcd8668c4bbc03595a88fe5d9f631c8d
Aiman Farhan <aimanfaruk98@gmail.com>
2024-11-18 23:55:23 +0800

    fix: setup log directory

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@
 
 require 'pathname'
 require 'readline'
+require 'fileutils'
 
 require 'bundler/setup'
 Bundler.require(:default)
@@ -10,3 +11,8 @@ APP_ROOT = Pathname.new(File.expand_path('..', __dir__))
 Dir[APP_ROOT.join('app/controllers/**/*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app/models/**/*.rb')].each { |file| require file }
 I18n.load_path += Dir["#{File.expand_path('config/locales')}/*.yml"]
+
+# Create log directory if it does not exist
+log_dir = File.dirname('log/production.log')
+FileUtils.mkdir(log_dir) unless Dir.exist?(log_dir)
+FileUtils.touch(log_dir) unless Dir.exist?(log_dir)
